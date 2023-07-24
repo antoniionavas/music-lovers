4feef9c89cb408e121af8b94b5886612d75a5d12
Antonio Navas <antonioluis_7_@hotmail.com>
2023-07-24 18:26:54 +0200

    planificacion propiedades y metodos casi terminada

--- js/game.js
+++ js/game.js
@@ -16,3 +16,33 @@ class Game{
 
  }
 }
+
+
+
+
+
+
+//PLANIFICACIÓN
+
+//PROPIEDADES
+//freddy -> x, y, h, w
+//questionsdiv -> x, y, h, w
+//answerdiv -> x, y, h, w
+//vidas -> x, y, h, w
+//score -> x, y, h, w
+
+//METODOS
+//posicionar las q&a
+//movimiento de las q&a
+//colision del personaje con la respuesta elegida por el user
+//movimiento del personaje
+//acción vidas (respuesta incorrecta )
+//accion score (respuesta correcta )
+//accion música x tiempo para elegir respuesta
+//gameover
+//reinicio
+
+//EXTRA
+//accion del personaje
+//score
+//tiempo restante para contestar
